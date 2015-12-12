06ba796f625d15b82e0674e614d4f3369395dacd
Sergey Zasenko <d3fin3@gmail.com>
2015-12-12 13:46:58 +0200

    open page in new tab by default

--- defaults/preferences/checkhost.js
+++ defaults/preferences/checkhost.js
@@ -1,6 +1,6 @@
 pref("extensions.checkhost.default_action", "http");
 pref("extensions.checkhost.slaves", 3);
-pref("extensions.checkhost.use_new_tab", false);
+pref("extensions.checkhost.use_new_tab", true);
 pref("extensions.checkhost.integration.flagfox", true);
 pref("extensions.checkhost.integration.context", true);
 pref("extensions.checkhost.integration.neterror", true);
